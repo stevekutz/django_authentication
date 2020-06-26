bb9650b16ef682e23417bec067c9a32f4e9b2efe
stevekutz <stkutz@gmail.com>
2020-06-25 22:03:57 -0500

    added login form, minor layout improvements

--- my_site/authenticate/views.py
+++ my_site/authenticate/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def home(request):
     return render(request, 'authenticate/home.html', {})
+
+
+def login_user(request):
+    return render(request, 'authenticate/login.html', {})  
